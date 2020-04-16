e26ceff71171bf9a6aa043b84fb32e94127ed3ec
江泽东 <36767299+fishb@users.noreply.github.com>
2020-04-16 14:19:09 +0800

    Update isWechat.js

--- common/isWechat.js
+++ common/isWechat.js
@@ -2,9 +2,10 @@
 export default function isWeiXin() {
     var ua = window.navigator.userAgent.toLowerCase();
     //mozilla/5.0 (iphone; cpu iphone os 9_1 like mac os x) applewebkit/601.1.46 (khtml, like gecko)version/9.0 mobile/13b143 safari/601.1
-    if (ua.match(/MicroMessenger/i) == 'micromessenger') {
-       return true;
-    } else {
-       return false;
-    }
+    return ua.match(/MicroMessenger/i) == 'micromessenger'
+//     if (ua.match(/MicroMessenger/i) == 'micromessenger') {
+//        return true;
+//     } else {
+//        return false;
+//     }
 }
